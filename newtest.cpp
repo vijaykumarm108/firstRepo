dbfa03050c27a3e707d1e31caf84de1191fb2b3f
vijay <vijaykumarm108@gmail.com>
2015-07-16 19:37:05 +0530

    Added newTest.cpp

---
+++ newtest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+cout << "Enter the Value you want to square root: ";
+
+int x;
+
+cin>> x;
+
+cout << " The spuare root of "<< x<< " = "<< sqrt(x)<<endl;
+}
+
